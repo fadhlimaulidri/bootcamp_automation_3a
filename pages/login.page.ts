b54b998898abe114c9eae2bcb504145816ee8914
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2026-07-12 11:13:32 +0700

    page object model login (#7)

---
+++ pages/login.page.ts
@@ -0,0 +1,31 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class LoginPage {
+  static new(page: Page) {
+    throw new Error('Method not implemented.');
+  }
+  // ===== PAGE INSTANCE =====
+  readonly page: Page;
+
+  // ===== LOCATORS =====
+  readonly emailField: Locator;
+  readonly passwordField: Locator;
+  readonly loginButton: Locator;
+
+  // ===== CONSTRUCTOR =====
+  constructor(page: Page) {
+    this.page = page;
+
+    // Initialize semua locator
+    this.emailField = page.locator("#email");
+    this.passwordField = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: 'Sign In' });
+  }
+
+  // ===== ACTION METHODS =====
+  async loginAs(email: string, password: string) {
+    await this.emailField.fill(email);
+    await this.passwordField.fill(password);
+    await this.loginButton.click();
+  }
+}
