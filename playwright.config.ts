a741b59b7bf16b624f75dd596385d4985daacbf2
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2026-08-01 16:42:53 +0700

    headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     trace: 'on-first-retry',     // Alat debug spesifik
     screenshot: 'only-on-failure',
     // video: 'retain-on-failure',
-    headless: false
+    headless: true
   },
 
   /* Configure projects for major browsers */
